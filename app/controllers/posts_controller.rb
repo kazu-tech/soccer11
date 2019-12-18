1c8a18ecd7a5c8899f559fdfff746c6eceb95aac
kazu-tech <o63.ishihara.kazuyuki@gmail.com>
2019-12-18 15:17:08 +0900

    テーブル変更

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -21,9 +21,9 @@ class PostsController < ApplicationController
   end
 
   def show
-    #@post = Post.find(params[:id])
+    @post = Post.find(params[:id])
     @team = Team.find(1)
-    @post = Post.find(1)
+    # @post = Post.find(1)
   end
 
   
--- db/schema.rb
+++ db/schema.rb
@@ -35,9 +35,9 @@ ActiveRecord::Schema.define(version: 2019_11_12_081225) do
 
   create_table "teams", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
+    t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
